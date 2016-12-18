e3888876a03da148bf144d735510b4b0aff4db77
prehensile <me@prehensile.co.uk>
2016-12-18 20:45:28 +0000

    improving sentence extractor

--- src/bingHandler.js
+++ src/bingHandler.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 
-var secrets = require( './secrets' );
+var secrets = require( './config/secrets' );
 
 var Bing = require('node-bing-api')({ accKey: secrets.BingKey });
 var Tokenizer = require('sentence-tokenizer');
@@ -8,16 +8,19 @@ var Tokenizer = require('sentence-tokenizer');
 
 var tokenizer = new Tokenizer( 'node-sentences' );
 
+var reClauses = new RegExp( /[,\;:\-\–\—\−]/gi );
+var reConjunctions = new RegExp( /(\band\b)|(\bor\b)/gi );
+
+
 function processItem( item, stem ){
     
     var statements = [];
     var d = item.description;
     
-    var i = d.indexOf( stem );
+    var reStem = new RegExp( stem, 'i' );
+    var i = d.search( reStem );
     if( i > -1 ){
         
-        //console.log( d );
-
         // trim description to stem and everything after
         d = d.substring( i );
         
@@ -29,36 +32,51 @@ function processItem( item, stem ){
         for( var ii in sentences ){
             
             var sentence = sentences[ii];
+            console.log( sentence );
 
-            // now, let's do some checks on this sentence.
-            var acceptSentence = true;
+            // now, let's do some checks on the sentence.
+            // if any fail, continue to the next sentence
+            // without adding to the statements array
 
             // only include sentences that include a stem
-            if( sentence.indexOf(stem) < 0 )
-                acceptSentence = false;
+            if( sentence.search( reStem ) < 0 )
+                continue;
 
-            // don't want any truncated sentences
-            if( sentence.indexOf( "..." ) > -1)
-                acceptSentence = false;
+            // if we've found a truncated sentence...
+            if( sentence.indexOf( "..." ) > -1 ){
+                var firstClause = reClauses.exec( sentence ) || [];
+                var firstConjoiner = reConjunctions.exec( sentence ) || [];
+                
+                // if we haven't matched any conjoiners or clauses in the sentence, bail
+                if( firstClause.length + firstConjoiner.length < 1 )
+                    continue;
+
+                // if we haven't bailed, use the first conjoiner or clause marker as a break point
+                var breakIndex = sentence.length;
+                if(firstClause.length > 0) breakIndex = Math.min( breakIndex, firstClause.index );
+                if(firstConjoiner.length > 0) breakIndex = Math.min( breakIndex, firstConjoiner.index );
+
+                // chop truncated sentence down to first clause
+                sentence = sentence.substring( 0, breakIndex );
+            }
 
             // don't want really short sentences
             // (split on a space to count words)
             if( sentence.split(' ').length < 3 )
-                acceptSentence = false;
+                continue;
 
             // quotations usually turn out weird
-            if( sentence.match(/['"“”‘’]/gi) )
-                acceptSentence = false;
+            if( sentence.match(/["“”]/gi) )
+                continue;
 
             // as do things with a lot of clauses
-            var m = sentence.match(/[,;:-–—−]/gi);
-            if( m && m.length > 1 )
-                acceptSentence = false;
+            var m = sentence.match( reClauses );
+            if( m && m.length > 3 )
+                continue;
 
-
-            // all checks done, push if acceptable
-            if( acceptSentence )
-                statements.push( sentence );
+            // if we're here, the sentence has passed all checks
+            // ( no continues have been hit)
+            statements.push( sentence );
         }
     }
 
@@ -100,6 +118,7 @@ function getStatementsWithStemAndSubject( stem, subject, callback ){
         
         ms = new Date().getTime() - ms;
         console.log( `-> bing query completed in ${ms}ms` );
+        console.log( res );
 
         if( error ){
             return callback( error, null );
@@ -109,8 +128,6 @@ function getStatementsWithStemAndSubject( stem, subject, callback ){
         callback( null, statements );
 
     });
-
-    //processItem({ description: "In the near future, sensing algorithms will achieve super-human performance for capabilities required for driving. Automated perception, including vision, is already near or at human-performance level for well-defined tasks such as recognition and tracking."}, stem);
 }
 
 function getStatementsForSubject( subject, callback ){
@@ -123,5 +140,6 @@ function getStatements( callback ){
 
 module.exports = {
     "getStatements" : getStatements,
-    "getStatementsForSubject" : getStatementsForSubject
+    "getStatementsForSubject" : getStatementsForSubject,
+    "processItem" : processItem
 };
